6a0a3eca9f6f7d17d8a6ffdc4ce48d4a946c4e8b
John Backus <johncbackus@gmail.com>
2017-03-05 17:38:58 -0800

    Add unit tests for runner

---
+++ spec/unit/yardcheck/runner_spec.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+RSpec.describe Yardcheck::Runner do
+  subject(:runner) { described_class.new(docs, observer) }
+
+  let(:observer) { Yardcheck::SpecObserver.new(observed_events)         }
+  let(:docs)     { Yardcheck::Documentation.new(YardcheckSpec::YARDOCS) }
+
+  let(:observed_events) do
+    [
+      {
+        type:     :call,
+        scope:    :instance,
+        method:   :add,
+        'module': TestApp::Namespace,
+        params:   { left: 'foo', right: 3 }
+      },
+      {
+        type:         :return,
+        scope:        :instance,
+        method:       :add,
+        'module':     TestApp::Namespace,
+        return_value: 5
+      },
+      {
+        type:     :call,
+        scope:    :class,
+        method:   :add,
+        'module': TestApp::Namespace.singleton_class,
+        params:   { left: 2, right: 3 }
+      },
+      {
+        type:         :return,
+        scope:        :class,
+        method:       :add,
+        'module':     TestApp::Namespace.singleton_class,
+        return_value: 5
+      }
+    ]
+  end
+
+  it 'compares the spec observations against the documentation' do
+    expect { runner.check }
+      .to output(<<~OUTPUT).to_stderr
+      Expected #<Class:TestApp::Namespace>#add to return String but observed Fixnum
+      Expected TestApp::Namespace#add to receive Integer for left but observed String
+      Expected TestApp::Namespace#add to return String but observed Fixnum
+      OUTPUT
+  end
+end
