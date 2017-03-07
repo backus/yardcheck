bb49d083698462ff9c2dddfa65d4cdf4244aa451
John Backus <johncbackus@gmail.com>
2017-03-06 23:32:40 -0800

    Extract color to file

--- lib/yardcheck/violation.rb
+++ lib/yardcheck/violation.rb
@@ -1,28 +1,4 @@
 module Yardcheck
-  module Color
-    private
-
-    def color(code, text)
-      "\e[#{code}m#{text}\e[0m"
-    end
-
-    def blue(text)
-      color(34, text)
-    end
-
-    def red(text)
-      color(31, text)
-    end
-
-    def yellow(text)
-      color(33, text)
-    end
-
-    def grey(text)
-      color(30, text)
-    end
-  end
-
   class Violation
     extend Color
     include Color
