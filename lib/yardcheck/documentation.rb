bd6591979adb23260ca205bb0fec281291198a6d
John Backus <johncbackus@gmail.com>
2017-03-22 20:04:04 -0700

    Add mutest:disable for Documentation

--- lib/yardcheck/documentation.rb
+++ lib/yardcheck/documentation.rb
@@ -4,6 +4,14 @@ module Yardcheck
   class Documentation
     include Concord.new(:yardocs), Memoizable
 
+    # mutest:disable
+    def self.parse
+      load_yard
+      new(YARD::Registry.all(:method))
+    end
+
+    # This is just YARD implementation details so I don't want to mutation cover this
+    # mutest:disable
     def self.load_yard
       # YARD doesn't write to .yardoc/ without this lock_for_writing and save
       YARD::Registry.lock_for_writing do
@@ -12,11 +20,6 @@ module Yardcheck
       end
 
       YARD::Registry.load!
-    end
-
-    def self.parse
-      load_yard
-      new(YARD::Registry.all(:method))
     end
 
     def method_objects
