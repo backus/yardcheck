e5b26e7eda1c53c91f4ab2ede0bf712a4b540583
John Backus <johncbackus@gmail.com>
2017-03-26 01:08:12 -0700

    Cleanup test app spec

--- test_app/spec/test_app_spec.rb
+++ test_app/spec/test_app_spec.rb
@@ -3,55 +3,57 @@
 require_relative './../lib/test_app'
 
 RSpec.describe TestApp::Namespace do
-  it 'does math with instance method' do
-    expect(TestApp::Namespace.new.add(2, 3)).to be(5)
-  end
+  let(:object) { described_class.new }
 
   it 'does math with singleton method' do
-    expect(TestApp::Namespace.add(2, 3)).to be(5)
+    expect(described_class.add(2, 3)).to be(5)
+  end
+
+  it 'does math with instance method' do
+    expect(object.add(2, 3)).to be(5)
   end
 
   it 'has an undocumented method and that is fine' do
-    expect(TestApp::Namespace.new.undocumented).to be(nil)
+    expect(object.undocumented).to be(nil)
   end
 
   it 'documents returning the parent but returns child' do
-    expect(TestApp::Namespace.new.returns_generic).to be_an_instance_of(TestApp::Namespace::Child)
+    expect(object.returns_generic).to be_an_instance_of(described_class::Child)
   end
 
   it 'documents returning a relative namespace incorrectly' do
-    expect(TestApp::Namespace.new.documents_relative).to be_a(String)
+    expect(object.documents_relative).to be_a(String)
   end
 
   it 'incorrectly documents a method as accepting Enumerable<String>' do
-    expect(TestApp::Namespace.new.enumerable_param('hi')).to be(nil)
+    expect(object.enumerable_param('hi')).to be(nil)
   end
 
   it 'properly tests a method with an instance double' do
-    expect(TestApp::Namespace.new.properly_tested_with_instance_double(instance_double(String))).to be(nil)
+    expect(object.properly_tested_with_instance_double(instance_double(String))).to be(nil)
   end
 
   it 'improperly tests a method with an instance double' do
-    expect(TestApp::Namespace.new.improperly_tested_with_instance_double(instance_double(Integer))).to be(nil)
+    expect(object.improperly_tested_with_instance_double(instance_double(Integer))).to be(nil)
   end
 
   it 'tests a method that raises an error instead of returning' do
-    expect { TestApp::Namespace.new.always_raise }.to raise_error(TestApp::Namespace::AppError)
+    expect { object.always_raise }.to raise_error(described_class::AppError)
   end
 
   it 'improperly documents the param with an invalid const' do
-    expect(TestApp::Namespace.new.ignoring_invalid_types('hi')).to be(nil)
+    expect(object.ignoring_invalid_types('hi')).to be(nil)
   end
 
   it 'returns a literal symbol' do
-    expect(TestApp::Namespace.new.returns_literal_symbol).to be(:foo)
+    expect(object.returns_literal_symbol).to be(:foo)
   end
 
   it 'returns a truthy value for predicate?' do
-    expect(TestApp::Namespace.new.truthy_predicate?).to be_truthy
+    expect(object.truthy_predicate?).to be_truthy
   end
 
   specify do
-    expect(TestApp::Namespace.new.tags_without_types(1)).to be(nil)
+    expect(object.tags_without_types(1)).to be(nil)
   end
 end
